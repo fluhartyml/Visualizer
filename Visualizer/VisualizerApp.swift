e8155bf9165e48a1c7f6df5cd1ee6c012eb36787
Michael Fluharty <michael@fluharty.com>
2025-11-25 10:19:18 -0600

    Initial Commit

---
+++ Visualizer/VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualizerApp.swift
+//  Visualizer
+//
+//  Created by Michael Fluharty on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisualizerTests/VisualizerTests.swift
@@ -0,0 +1,16 @@
+//
+//  VisualizerTests.swift
+//  VisualizerTests
+//
+//  Created by Michael Fluharty on 11/25/25.
+//
+
+import Testing
+
+struct VisualizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
